e50041a47b76c042949c3e7460fb57d7bba99e99
BloodhunterD <backup-agent@bloodhunterd.com>
2019-10-08 21:01:37 +0200

    BackupAgent and Configuration exception added

---
+++ app/src/Exceptions/BackupAgent.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Backup Agent project.
+ * Visit project at https://github.com/bloodhunterd/backup-agent
+ *
+ * © BloodhunterD <backup-agent@bloodhunterd.com> | 2019
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Backup\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class BackupAgent
+ *
+ * @author BloodhunterD
+ *
+ * @package Backup\Exceptions
+ */
+class BackupAgent extends RuntimeException
+{
+
+}
