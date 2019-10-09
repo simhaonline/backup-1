56161fd5a78891eeddcac649b61e213f074b6c51
BloodhunterD <backup-agent@bloodhunterd.com>
2019-10-09 20:54:56 +0200

    Compressible interface added.

---
+++ agent/src/Interfaces/Compressible.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the Backup project.
+ * Visit project at https://github.com/bloodhunterd/backup
+ *
+ * © BloodhunterD <backup@bloodhunterd.com> | 2019
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Backup\Interfaces;
+
+/**
+ * Interface Compressible
+ *
+ * @author BloodhunterD
+ *
+ * @package Backup\Interfaces
+ */
+interface Compressible
+{
+
+    /**
+     * Get the name
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Get the source directory
+     *
+     * @return string
+     */
+    public function getSource(): string;
+
+    /**
+     * Get the archive path
+     *
+     * @return string
+     */
+    public function getArchive(): string;
+}
